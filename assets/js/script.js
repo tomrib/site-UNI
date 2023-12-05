32e7114452d5797769dc32a848f73ebf3d2ed842
Tommy Ribeiro <115786099+tomrib@users.noreply.github.com>
2023-12-05 11:03:36 +0100

    nettoyage de code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,3 @@
-
-    // Sélectionnez le formulaire
-    var form = document.querySelector('#submitForm');
-
-    // Ajoutez un gestionnaire d'événements pour l'événement de soumission du formulaire
-    form.addEventListener('click', function (event) {
-        // Empêche le comportement par défaut du formulaire (rafraîchissement de la page)
-        form.preventDefault();
-
-        // Ajoutez ici le code pour traiter la soumission du formulaire, par exemple, envoi de données via AJAX
-
-        // Vous pouvez également fermer la fenêtre modale si nécessaire
-        var modal = new bootstrap.Modal(document.getElementById('request'));
-        modal.hide();
-    });
-
-
+function btn() {
+    e.preventDefault();
+};
